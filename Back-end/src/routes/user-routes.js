92cd93a1f870c4f15a1e83c606248e66029c9879
Содмандах Цогтсайхан <HP230379@G303-CC0147.local>
2024-03-27 21:20:33 +0800

    sodmandak's change

--- Back-end/src/routes/user-routes.js
+++ Back-end/src/routes/user-routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, getAllUsers, getUser, forgotPassword, resetPassword } from "../controllers/user-controller.js";
+import { createUser, getAllUsers, getUser, forgotPassword, resetPassword, verifyCode } from "../controllers/user-controller.js";
 import { login } from "../controllers/login-controller.js";
 import { verifyToken } from "../middleWare/authenticator.js";
 
@@ -11,3 +11,4 @@ userRouter.post('/signUp', createUser);
 userRouter.post('/logIn', login);
 userRouter.post('/forgot-password', forgotPassword);
 userRouter.post('/reset-password', resetPassword);
+userRouter.post('/verifyCode', verifyCode);
