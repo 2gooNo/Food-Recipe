3bbb0c888a948142e594f8f5e78a8d4293024198
Tuguldur Dashrentsen <HP230089@G303-CC0131.local>
2024-04-10 19:17:40 +0800

    tuguldur

--- Front-end/src/app/HomePage/page.js
+++ Front-end/src/app/HomePage/page.js
@@ -24,7 +24,6 @@ export default function HomePage() {
     const foodData = await axios.get(`${Back_End_Url}/getAllFood`);
     console.log(foodData);
     generateRandomNumbers(foodData);
-    // generateSixNumbers(foodData)
     setFoods(foodData);
   };
 
