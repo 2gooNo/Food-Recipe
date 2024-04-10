7600166ce31c448bc3f64c242661b7657e43ef65
Энэрэл Батнасан <HP230307@G303-CC0156.local>
2024-04-10 15:45:25 +0800

    Your commit message here

---
+++ Back-end/src/controllers/category-controller.js
@@ -0,0 +1,22 @@
+import Category from "../models/category-model.js";
+
+// Controller to handle creating a new category
+export const createCategory = async (req, res) => {
+  try {
+    const category = new Category(req.body);
+    await category.save();
+    res.status(201).json(category);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Controller to handle getting all categories
+export const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import { userRouter } from "./routes/user-routes.js";
 import { foodRouter } from "./routes/food-routes.js";
-import mongoose from "mongoose";
+import { categoryRouter } from "./routes/category-routers.js";
 
 const app = express();
 
-const port = 8001;
+app.use(cors());
+app.use(express.json());
+
+const port = 8000;
 
 app.use(cors());
 app.use(express.json());
@@ -14,21 +18,35 @@ app.use(express.json());
 app.use(foodRouter);
 app.use(userRouter);
 
+app.use(foodRouter);
+app.use(userRouter);
+app.use(categoryRouter);
+
 const connectFoodDB = async () => {
   await mongoose.connect(
     "mongodb+srv://Food-Recipe:food-recipe@food-recipe.bf2uk28.mongodb.net/"
   );
   console.log("database1 connected");
 };
+// try {
+//   await mongoose.connect(
+//     "mongodb+srv://Food-Recipe:food-recipe@food-recipe.bf2uk28.mongodb.net/Food"
+//   );
+//   console.log("MongoDB connected...");
+// } catch (error) {
+//   console.error(error.message);
+//   process.exit(1);
+// }
 
 // const connectUserDB = async () => {
 //   await mongoose.connect(
-//     "mongodb+srv://Food-Recipe:food-recipe@food-recipe.bf2uk28.mongodb.net/"
+//     "mongodb+srv://Food-Recipe:food-recipe@food-recipe.bf2uk28.mongodb.net/User"
 //   );
 //   console.log("database2 connected");
 // };
 
 connectFoodDB();
+// connectDB();
 // connectUserDB();
 
 app.listen(port, () => {
