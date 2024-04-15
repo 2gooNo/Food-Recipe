811096a4bf4a1efe456396b7cce892a8e957f780
Энэрэл Батнасан <HP230307@G303-CC0156.local>
2024-04-15 17:21:45 +0800

    Category

--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -28,26 +28,8 @@ const connectFoodDB = async () => {
   );
   console.log("database1 connected");
 };
-// try {
-//   await mongoose.connect(
-//     "mongodb+srv://Food-Recipe:food-recipe@food-recipe.bf2uk28.mongodb.net/Food"
-//   );
-//   console.log("MongoDB connected...");
-// } catch (error) {
-//   console.error(error.message);
-//   process.exit(1);
-// }
-
-// const connectUserDB = async () => {
-//   await mongoose.connect(
-//     "mongodb+srv://Food-Recipe:food-recipe@food-recipe.bf2uk28.mongodb.net/User"
-//   );
-//   console.log("database2 connected");
-// };
 
 connectFoodDB();
-// connectDB();
-// connectUserDB();
 
 app.listen(port, () => {
   console.log("Server is running on http://localhost:" + port);
--- Back-end/src/routes/user-routes.js
+++ Back-end/src/routes/user-routes.js
@@ -1,15 +1,25 @@
 import express from "express";
-import { createUser, getAllUsers, getUser, forgotPassword, resetPassword, verifyCode, UpdatePassword } from "../controllers/user-controller.js";
+import {
+  createUser,
+  getAllUsers,
+  getUser,
+  forgotPassword,
+  resetPassword,
+  verifyCode,
+  UpdatePassword,
+  deleteUser,
+} from "../controllers/user-controller.js";
 import { login } from "../controllers/login-controller.js";
 import { verifyToken } from "../middleWare/authenticator.js";
 
 export const userRouter = express.Router();
 
-userRouter.get('/users', verifyToken, getAllUsers);
-userRouter.get('/getUser', verifyToken, getUser);
-userRouter.post('/signUp', createUser);
-userRouter.post('/logIn', login);
-userRouter.post('/forgot-password', forgotPassword);
-userRouter.post('/reset-password', resetPassword);
-userRouter.post('/verifyCode', verifyCode);
-userRouter.post('/UpdatePassword', UpdatePassword)
+userRouter.get("/users", verifyToken, getAllUsers);
+userRouter.get("/getUser", verifyToken, getUser);
+userRouter.post("/deleteUser", verifyToken, deleteUser);
+userRouter.post("/signUp", createUser);
+userRouter.post("/logIn", login);
+userRouter.post("/forgot-password", forgotPassword);
+userRouter.post("/reset-password", resetPassword);
+userRouter.post("/verifyCode", verifyCode);
+userRouter.post("/UpdatePassword", UpdatePassword);
